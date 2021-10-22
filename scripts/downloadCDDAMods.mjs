ed147ffa791248aba0a6a77de3f6f376c0dd867e
林东吴 <lindongwu@bytedance.com>
2021-10-22 23:25:20 +0800

    feat: see if we have all files already, if so, skip this script to save bandwidth

--- scripts/downloadCDDAMods.mjs
+++ scripts/downloadCDDAMods.mjs
@@ -1,6 +1,8 @@
+/* eslint-disable unicorn/no-process-exit */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
+import fs from 'fs-jetpack';
 import path from 'path';
 import fetch from 'node-fetch';
 import { pipeline } from 'stream';
@@ -8,12 +10,19 @@ import { promisify } from 'util';
 import dotenv from 'dotenv';
 import unzipper from 'unzipper';
 
+const unzipOutputPath = path.join(__dirname, '..', 'data');
+
+const folderNames = ['Cataclysm-DDA-master', 'Kenan-Modpack-Chinese'].map((p) => path.join(unzipOutputPath, p));
+// see if we have all files already, if so, skip this script to save bandwidth
+if (await Promise.all(folderNames.map((p) => fs.existsAsync(p)))) {
+  console.log('All file existed');
+  process.exit(0);
+}
+
 const cddaCoreZipUrl = 'https://github.com/CleverRaven/Cataclysm-DDA/archive/refs/heads/master.zip';
 const kenanPackZipUrl = 'https://github.com/linonetwo/CDDA-Kenan-Modpack-Chinese/releases/latest/download/Kenan-Modpack-Mod.zip';
 
-const unzipOutputPath = path.join(__dirname, '..', 'data');
 const streamPipeline = promisify(pipeline);
-
 dotenv.config();
 
 async function downloadTo(zipUrl, folder) {
