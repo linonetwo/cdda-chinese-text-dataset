5a61b972a39a2c684ac992e0243df0c3573c542a
Lin Onetwo <linonetwo012@gmail.com>
2021-10-23 15:32:40 +0800

    feat: provide type

--- src/cddaJSONWithNameAndDescription.ts
+++ src/cddaJSONWithNameAndDescription.ts
@@ -1,7 +1,12 @@
 import preval from 'preval.macro';
 
+export interface ICDDAJSONWithNameAndDescription {
+  description: string;
+  name: string;
+  type: string;
+}
 export const cddaJSONWithNameAndDescription = preval`
 const fs = require('fs');
   const path = require('path');
   module.exports = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', 'translatedNameDesc.json'), 'utf-8'));
-` as string;
+` as ICDDAJSONWithNameAndDescription[];
